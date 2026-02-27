2a3a81130db18ab17020f6c5d0ed676a096b7d84
williammuji <williammuji@gmail.com>
2026-02-27 22:26:00 +0800

    DriverIntegritySensor refactor

--- sensors/DriverIntegritySensor.cpp
+++ sensors/DriverIntegritySensor.cpp
@@ -4,13 +4,9 @@
 #include "../utils/SystemUtils.h"
 #include "../Logger.h"
 #include "../utils/Utils.h"
-#include "../CheatConfigManager.h"
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <psapi.h>
-#include <wintrust.h>
-#include <softpub.h>
 
 #pragma comment(lib, "wintrust.lib")
 #pragma comment(lib, "psapi.lib")
@@ -26,47 +22,28 @@ SensorExecutionResult DriverIntegritySensor::Execute(SensorRuntimeContext &conte
         int cDrivers = cbNeeded / sizeof(drivers[0]);
         if (cDrivers == 0)
         {
-             LOG_WARNING(AntiCheatLogger::LogCategory::SENSOR, "DriverIntegritySensor: No drivers found (Insufficient privileges?)");
+             LOG_WARNING(AntiCheatLogger::LogCategory::SENSOR, "DriverIntegritySensor: No drivers found");
              RecordFailure(anti_cheat::SYSTEM_API_CALL_FAILED);
              return SensorExecutionResult::FAILURE;
         }
+
+        SystemUtils::WindowsVersion winVer = SystemUtils::GetWindowsVersion();
+
         for (int i = 0; i < cDrivers; i++)
         {
-             // Throttling check? Original code didn't have it in the snippet 2737-2829.
-             // Maybe it was short enough.
-
             WCHAR szDriver[MAX_PATH];
             if (GetDeviceDriverFileNameW(drivers[i], szDriver, MAX_PATH))
             {
                 std::wstring driverPath = SystemUtils::NormalizeKernelPathToWinPath(szDriver);
 
-                // Simple logic: verify signature
-                std::wstring normalizedPath = driverPath;
-                std::transform(normalizedPath.begin(), normalizedPath.end(), normalizedPath.begin(), ::towlower);
-                const std::wstring driverName = Utils::GetFileName(normalizedPath);
+                // Use unified ValidateModule logic
+                auto validation = Utils::ValidateModule(driverPath, winVer);
 
-                // Coarse-grained unified whitelist:
-                // 1) Global module whitelist policy (dirs/files/system)
-                // 2) Existing "system modules" name whitelist
-                bool isWhitelisted = Utils::IsWhitelistedModule(normalizedPath);
-                if (!isWhitelisted)
-                {
-                    auto systemModules = CheatConfigManager::GetInstance().GetWhitelistedSystemModules();
-                    if (systemModules && systemModules->count(driverName) > 0)
-                    {
-                        isWhitelisted = true;
-                    }
-                }
-
-                if (isWhitelisted)
-                {
-                    continue;
-                }
-
-                if (!VerifyDriverSignature(driverPath))
+                if (!validation.isTrusted)
                 {
                     std::string u8Path = Utils::WideToString(driverPath);
-                    context.AddEvidence(anti_cheat::ENVIRONMENT_SUSPICIOUS_DRIVER, "Unsigned or untrusted driver: " + u8Path);
+                    context.AddEvidence(anti_cheat::ENVIRONMENT_SUSPICIOUS_DRIVER,
+                        "Suspicious driver (Reason: " + validation.reason + "): " + u8Path);
                 }
             }
         }
@@ -78,40 +55,4 @@ SensorExecutionResult DriverIntegritySensor::Execute(SensorRuntimeContext &conte
     }
 
     return SensorExecutionResult::SUCCESS;
-}
-
-bool DriverIntegritySensor::VerifyDriverSignature(const std::wstring& filePath)
-{
-   LONG lStatus;
-   WINTRUST_FILE_INFO FileData;
-   memset(&FileData, 0, sizeof(FileData));
-   FileData.cbStruct = sizeof(WINTRUST_FILE_INFO);
-   FileData.pcwszFilePath = filePath.c_str();
-   FileData.hFile = NULL;
-   FileData.pgKnownSubject = NULL;
-
-   GUID WVTPolicyGUID = WINTRUST_ACTION_GENERIC_VERIFY_V2;
-   WINTRUST_DATA WinTrustData;
-   memset(&WinTrustData, 0, sizeof(WinTrustData));
-   WinTrustData.cbStruct = sizeof(WinTrustData);
-   WinTrustData.pPolicyCallbackData = NULL;
-   WinTrustData.pSIPClientData = NULL;
-   WinTrustData.dwUIChoice = WTD_UI_NONE;
-   WinTrustData.fdwRevocationChecks = WTD_REVOKE_NONE;
-   WinTrustData.dwUnionChoice = WTD_CHOICE_FILE;
-   WinTrustData.dwStateAction = WTD_STATEACTION_VERIFY;
-   WinTrustData.hWVTStateData = NULL;
-   // WinTrustData.pwszURLReference = NULL; // Removing this line as it might be problematic if not zeroed (it is zeroed by memset)
-   WinTrustData.dwProvFlags = WTD_SAFER_FLAG;
-   WinTrustData.dwUIContext = 0;
-   WinTrustData.pFile = &FileData;
-
-   lStatus = WinVerifyTrust(NULL, &WVTPolicyGUID, &WinTrustData);
-
-   bool isTrusted = (lStatus == ERROR_SUCCESS);
-
-   WinTrustData.dwStateAction = WTD_STATEACTION_CLOSE;
-   WinVerifyTrust(NULL, &WVTPolicyGUID, &WinTrustData);
-
-   return isTrusted;
 }
